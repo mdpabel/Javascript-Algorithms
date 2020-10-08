02a9ec9f59fb1400b3c571901fa60c3341db5f14
mdpabel <mdpabel385@gmail.com>
2020-10-08 13:19:33 +0600

    using time by performance.now() function

---
+++ Big O Notation/add.js
@@ -0,0 +1,33 @@
+// Using loop
+function add(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+const loopt1 = performance.now();
+"add", add(100000000000000000000000);
+const loopt2 = performance.now();
+
+console.log(t2 - t5);
+
+// Using Math
+function add2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+const t3 = performance.now();
+add2(100000000000000000000000);
+const t4 = performance.now();
+
+console.log(t4 - t3);
+
+/*** THE PROBLEM WITH TIME ***
+ * Different machine will record different times
+ * The same machine will record different times
+ * For fast algorithm, speed measurements may not be precise enough.
+ */
+
+// That's where BIG O notation comes in
