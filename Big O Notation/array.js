0eea6c53cbe7cec31f9916846e8926d50dd20fe7
mdpabel <mdpabel385@gmail.com>
2020-10-12 18:52:43 +0600

    object & array

---
+++ Big O Notation/array.js
@@ -0,0 +1,13 @@
+/**
+ * push - O(1)
+ * pop - O(1)
+ * shift - O(n)
+ * unshift - O(n)
+ * concat - O(n)
+ * slice - O(n)
+ * splice -O(n)
+ * sort - O(n * logn)
+ * forEach, filter, map, reduce - O(n)
+ */
+
+// searching - O(n)
---
+++ Big O Notation/object.js
@@ -0,0 +1,17 @@
+// Big(o) of objects
+
+/**
+ * Insertion - O(1)
+ * deletion - O(1)
+ * Searching - O(n)
+ * Access - O(1)
+ */
+
+// Methods
+
+/**
+ * Object.keys - O(n)
+ * Object.values - O(n)
+ * Object.entries - O(n)
+ * hasOwnProperty - O(1)
+ */
